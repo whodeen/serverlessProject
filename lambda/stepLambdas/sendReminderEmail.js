b3a9b9aa683acc1499f395b8a54f11fa5d2cdd4d
Alexander Fedoniuk <whodeen@gmail.com>
2021-05-22 18:17:11 +0300

    Created sendReminderEmail function

---
+++ lambda/stepLambdas/sendReminderEmail.js
@@ -0,0 +1,33 @@
+import * as AWS from 'aws-sdk'
+
+const SES = new AWS.SES();
+
+exports.handler = async event => {
+    console.log('event', event);
+
+    const email = event.Input.Payload.email;
+
+    const message = `Hi from step functions!`;
+
+    const params = {
+        Destination: {
+            ToAddresses: [email]
+        },
+        Message: {
+            Body: {
+                Text: { Data: message }
+            },
+            Subject: { Data: 'Reminder' }
+        },
+        Source: 'whodeen@gmail.com'
+    }
+
+    try {
+        await SES.sendEmail(params).promise();
+        return;
+    } catch (err) {
+        console.log('error sending email', err);
+        throw err;
+    }
+
+}
