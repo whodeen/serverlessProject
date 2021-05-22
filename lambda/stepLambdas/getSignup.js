4e97794cc29f1ee7cccbf5d1e53119c962b01b56
Alexander Fedoniuk <whodeen@gmail.com>
2021-05-22 18:06:19 +0300

    Created getSignup function

---
+++ lambda/stepLambdas/getSignup.js
@@ -0,0 +1,14 @@
+import Dynamo from "../common/Dynamo";
+
+const tableName = process.env.signupTableName;
+
+exports.handler = async event => {
+    console.log(event);
+
+    const ID = event.Input.Payload.ID;
+    const ID = uuid();
+
+    const row = await Dynamo.get(ID, tableName);
+
+    return row;
+}
